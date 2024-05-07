b11bbd495e8e1bc07fdddeb62ccc4eb234cd215b
Cristian Mozetich <cristianmozetich@gmail.com>
2024-05-07 21:14:03 +0100

    detalles footer

--- frontend/src/app/components/contact/Contact.jsx
+++ frontend/src/app/components/contact/Contact.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import EmailIcon from "../icons/email";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 
 const Contact = () => {
+  const t = useTranslations();
+
   return (
       <div className="flex flex-col items-center mt-8 p-6 w-3/4">
         <h1 className="text-2xl tracking-wider border-b border-green-500 font-bold">CONTÁCTAME</h1>
-        <span className="p-3 tracking-wider text-xl text-center">
-          Actualmente estoy en busqueda de nuevas oportunidades y propuestas de trabajo
+        <span className="p-3 tracking-wider text-lg text-center">
+          {t("footer")}
         </span>
         <a
           href="mailto:cristianmozetich@gmail.com"
           target="_blank"
-          className="bg-slate-300 h-12 w-36 rounded-lg text-gray-700 flex gap-2 justify-center items-center p-2 text-md hover:bg-emerald-100 tracking-widest font-bold cursor-pointer transition-transform duration-300 transform hover:-translate-y-1 hover:-translate-x-1 hover:shadow-sm hover:shadow-emerald-200 dark:hover:bg-sky-200 dark:hover:shadow-sky-200"
+          className="bg-blue-500 hover:bg-blue-600 text-white h-12 w-36 rounded-lg dark:text-gray-700 flex gap-2 justify-center items-center p-2 text-md  tracking-widest cursor-pointer transition-transform duration-300 transform hover:translate-x-1 hover:shadow-sm  dark:hover:bg-slate-200  dark:bg-slate-300 "
         >
           Email <EmailIcon />
         </a>
+        <span className="text-sm mt-10 p-5 text-center">
+        {t("footer2")}
+        <Link className="dark:text-green-500 text-emerald-600 text-lg m-2" target="_blank" href={"https://github.com/CristianMozetich"}>
+          Cristian Mozetich
+        </Link>
+      </span>
       </div>
   );
 };
--- frontend/src/app/components/footer/Footer.jsx
+++ frontend/src/app/components/footer/Footer.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Skills from "../skills/Skills";
 import Contact from "../contact/Contact";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 const Footer = () => {
+  const t = useTranslations();
   return (
     <div className="bg-slate-300 dark:bg-slate-800 w-full h-full items-center flex flex-col">
       <Skills />
